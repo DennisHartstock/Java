c77654a9926860ecd4f520b445db5de74b3a7f43
DennsHartstock <dennis.hartstock@web.de>
2021-01-17 12:33:40 +0100

    Сейчас 3126 год. Мой друг родился 8 лет назад. Написать программу, выводящую на экран год рождения моего друга.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task01.task0103;                                                  
+
+public class Solution {
+    public static void main(String[] args) {
+        int currentYear = 3126;
+        int yearOfBirthOfMyFriend = currentYear - 8;
+        System.out.println(yearOfBirthOfMyFriend);
+    }
+}
