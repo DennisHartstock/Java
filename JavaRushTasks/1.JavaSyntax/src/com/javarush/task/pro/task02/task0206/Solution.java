7020d66b607a2c74c7e3d9f1541936e6178b28d2
DennsHartstock <dennis.hartstock@web.de>
2021-01-18 09:06:36 +0100

    В методе main объяви переменные word, phrase, line и text типа String. Присвой им разные значения. Используй одну команду для создания и инициализации переменных. Значениями могут быть любые строки

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task02.task0206;                                                  
+                                                  
+public class Solution {                                                  
+                                                  
+    public static void main(String[] args) {                                                  
+       String word = "My ", phrase = "name ", line = "is ", text = "Dennis";
+    }                                                  
+}
