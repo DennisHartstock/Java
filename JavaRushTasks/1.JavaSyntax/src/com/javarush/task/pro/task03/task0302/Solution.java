9203d2f3de49044cd4aa91254f096154f703857c
DennsHartstock <dennis.hartstock@web.de>
2021-01-19 09:45:18 +0100

    Ввести с клавиатуры имя и возраст. Если возраст в пределах 18-28 (включительно), то вывести надпись "Имя, явитесь в военкомат", где Имя - это имя, введенное ранее с клавиатуры.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите Ваше имя: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Введите Ваш возраст: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18 && age <= 28) {
+            System.out.println(name + ", явитесь в военкомат");
+        }
+    }
+}
