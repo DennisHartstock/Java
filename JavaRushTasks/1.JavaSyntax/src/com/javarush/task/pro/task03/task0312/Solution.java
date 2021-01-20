4de00d019430409d448f525e5f91c95c381784ea
DennsHartstock <dennis.hartstock@web.de>
2021-01-20 09:52:36 +0100

    Напиши программу, которая считывает с клавиатуры две строки и выдает сообщение о том, одинаковые ли эти строки.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task03.task0312;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Введите текст: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String string1 = scanner.nextLine();
+        String string2 = scanner.nextLine();
+
+        if (string1.equals(string2))
+            System.out.println("строки одинаковые");
+        else
+            System.out.println("строки разные");
+    }
+}
