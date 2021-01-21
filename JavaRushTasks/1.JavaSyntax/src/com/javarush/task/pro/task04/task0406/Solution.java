1443446af0790cf0e053caaae9b98877f924681b
DennsHartstock <dennis.hartstock@web.de>
2021-01-21 09:40:55 +0100

    Вводим данные с клавиатуры и сразу их выводим на экран, пока не будет введено слово "enough". Слово "enough" выводить не нужно. Для этого необходимо использовать бесконечный цикл (while(true)).
    Подсказка: чтобы завершить бесконечный цикл, используй оператор break.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0406;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String string = scanner.nextLine();
+            if (string.equals("enough")) {
+                break;
+            }
+            System.out.println(string);
+        }
+    }
+}
