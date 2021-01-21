0764e3a7f976f16fc0bd3283f2d88a8923c2fcf5
DennsHartstock <dennis.hartstock@web.de>
2021-01-21 09:59:10 +0100

    Вывести на экран сумму чисел от 1 до 100 включительно, не кратных 3. Для этого используй цикл while.
    Подсказка: чтобы в цикле перейти к следующему числу, используй оператор continue.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task04/task0407/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task04.task0407;
+
+public class Solution {
+    public static void main(String[] args) {
+        int n = 0;
+        int sum = 0;
+
+        while (n <= 100) {
+            if (n % 3 == 0) {
+                n++;
+                continue;
+            } else {
+                sum += n;
+                n++;
+            }
+        }
+        System.out.println(sum);
+    }
+}
