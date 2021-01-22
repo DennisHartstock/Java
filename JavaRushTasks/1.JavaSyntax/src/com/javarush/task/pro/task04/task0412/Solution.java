b93e60ada2eba67a847908515af117f70166895d
DennsHartstock <dennis.hartstock@web.de>
2021-01-22 12:35:54 +0100

    Напиши программу, которая выводит на экран сумму чисел от 1 до 17, кратных 3. Для этого используй цикл for. Подсказка: чтобы в цикле перейти к следующему числу, используй оператор continue.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task04/task0412/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task04.task0412;
+
+public class Solution {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i < 17 ; i++) {
+            if (i % 3 != 0) {
+                continue;
+            }
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
