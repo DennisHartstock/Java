ad88d9beb0803420dc67cebe8c5ef3c2e301eee6
DennsHartstock <dennis.hartstock@web.de>
2021-01-26 09:40:24 +0100

    В методе main(String[]) тебе нужно заполнить массив strings значениями. Если индекс массива чётный — присвоить значение "Чётный" (ноль — цифра чётная), иначе присвоить "Нечётный". Вывод текста в консоль в тестировании не участвует.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task05/task0502/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task05.task0502;
+
+public class Solution {
+    public static final String ODD = "Нечётный";
+    public static final String EVEN = "Чётный";
+    public static String[] strings = new String[20];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < strings.length; i++) {
+            if (strings.length % 2 == 0)
+                strings[i] = EVEN;
+            else
+                strings[i] = ODD;
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print("index = " + i);
+            System.out.println(" value = " + strings[i]);
+        }
+    }
+}
