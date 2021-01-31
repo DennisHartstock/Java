14e4f2f6d656de9e457f68d552425d8ec3182f8b
DennsHartstock <dennis.hartstock@web.de>
2021-01-31 08:50:45 +0100

    Неплохо было бы иметь один метод для решения различных задач. У тебя есть возможность написать такой. Создай еще 9 методов universalMethod(). В сумме их должно получиться 10. Параметры, которые они должны принимать, придумай сам.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.pro.task06.task0606;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    public static void universalMethod(short x) {
+
+    }
+
+    public static void universalMethod(int x) {
+
+    }
+
+    public static void universalMethod(long x) {
+
+    }
+
+    public static void universalMethod(short x, String string) {
+
+    }
+
+    public static void universalMethod(int x, String string) {
+
+    }
+
+    public static void universalMethod(long x, String string) {
+
+    }
+
+    public static void universalMethod(String sting) {
+
+    }
+
+    public static void universalMethod(boolean isTrue) {
+
+    }
+
+    public static void universalMethod(char code) {
+
+    }
+}
