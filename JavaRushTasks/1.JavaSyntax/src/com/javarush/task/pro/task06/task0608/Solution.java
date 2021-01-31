43eaa16d7993a5459348ef9e176d9e4c96be1064
DennsHartstock <dennis.hartstock@web.de>
2021-01-31 09:08:49 +0100

    Давай напишем реализацию калькулятора, который будет считать степени чисел. Для этого создай метод cube(). В качестве аргумента он должен принимать целочисленное значение типа long. Метод должен возводить полученное значение в третью степень и возвращать его как результат работы метода. Числа, которыми придется оперировать, могут быть большими. Поэтому метод cube() должен возвращать тип long.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task06.task0608;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long x) {
+        return x * x * x;
+    }
+
+}
