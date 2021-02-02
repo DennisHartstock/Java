f54a56a0aa86f3b85364aeb01cc079db9f692525
DennsHartstock <dennis.hartstock@web.de>
2021-02-02 07:24:38 +0100

    Создай по одной переменной каждого примитивного типа в классе Solution. Переменные не должны находиться в каком-либо методе.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task07.task0701;
+
+public class Solution {
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+
+    float aFloat;
+    double aDouble;
+
+    boolean aBoolean;
+    char aChar;
+}
