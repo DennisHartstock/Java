8748692d975ccc9abf91b78b04ff3e5a5ff3178b
DennsHartstock <dennis.hartstock@web.de>
2021-02-03 11:07:45 +0100

    Используя оператор new в методе main(), создай:
    10 объектов типа String;
    5 объектов типа int[] (массивов значений int);
    2 объекта типа Scanner.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task07.task0712;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = new String();
+        String string1 = new String();
+        String string2 = new String();
+        String string3 = new String();
+        String string4 = new String();
+        String string5 = new String();
+        String string6 = new String();
+        String string7 = new String();
+        String string8 = new String();
+        String string9 = new String();
+
+        int[] array = new int[0];
+        int[] array1 = new int[0];
+        int[] array2 = new int[0];
+        int[] array3 = new int[0];
+        int[] array4 = new int[0];
+
+        Scanner scanner = new Scanner("");
+        Scanner scanner1 = new Scanner("");
+    }
+}
