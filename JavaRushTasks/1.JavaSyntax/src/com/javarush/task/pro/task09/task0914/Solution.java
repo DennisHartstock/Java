b7b1d371238509db78ab3ee6924789c87b0ef144
DennsHartstock <dennis.hartstock@web.de>
2021-02-09 10:13:29 +0100

    Реализуй метод changePath(String, String) так, чтобы он заменял версию jdk в пути, полученном первым параметром метода, на версию, полученную вторым параметром, и возвращал новый путь. Версия jdk расположена между третьим и четвертым "/".

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task09.task0914;
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        String[] strings = path.split("/");
+        return path.replace(strings[3], jdk);
+    }
+}
