a0ec27dc5e86efc0655197f8542516ae09f853ca
DennsHartstock <dennis.hartstock@web.de>
2021-02-10 10:21:33 +0100

    Используя StringTokenizer раздели query на части по разделителю delimiter.
    Пример:
    getTokens("com.javarush.task.pro.task09.task0915", ".a")
    возвращает массив строк
    {"com", "j", "v", "rush", "t", "sk", "pro", "t", "sk09", "t", "sk0915"}

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task09/task0915/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task09.task0915;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        String packagePath = "com.javarush.task.pro.task09.task0915";
+        String[] tokens = getTokens(packagePath, ".a");
+        System.out.println(Arrays.toString(tokens));
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        String[] result = new String[tokenizer.countTokens()];
+        int i = 0;
+        while (tokenizer.hasMoreTokens()) {
+            result[i++] = tokenizer.nextToken();
+        }
+        return result;
+    }
+}
