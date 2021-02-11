70837a94a31a89bd16005b1a42727a90b0b8a9ce
DennsHartstock <dennis.hartstock@web.de>
2021-02-11 08:56:21 +0100

    Давай построим жилой комплекс из трех высоток. Для этого мы будем использовать три разных способа вывода информации: Объявляет о результате постройки. Объявляет о результате и указывает количество этажей. Объявляет о результате и указывает застройщика.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task10/task1002/Skyscraper.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task10.task1002;
+
+public class Skyscraper {
+    public static final String SKYSCRAPER_WAS_BUILD = "Небоскреб построен.";
+    public static final String SKYSCRAPER_WAS_BUILD_FLOORS_COUNT = "Небоскреб построен. Количество этажей - ";
+    public static final String SKYSCRAPER_WAS_BUILD_DEVELOPER = "Небоскреб построен. Застройщик - ";
+
+    public Skyscraper() {
+        System.out.println(SKYSCRAPER_WAS_BUILD);
+    }
+
+    public Skyscraper(int countOfFloors) {
+        System.out.println(SKYSCRAPER_WAS_BUILD_FLOORS_COUNT + countOfFloors);
+    }
+
+    public Skyscraper(String developer) {
+        System.out.println(SKYSCRAPER_WAS_BUILD_DEVELOPER + developer);
+    }
+
+    public static void main(String[] args) {
+        var skyscraper = new Skyscraper();
+        var skyscraperTower = new Skyscraper(50);
+        var skyscraperSkyline = new Skyscraper("JavaRushDevelopment");
+    }
+}
+
