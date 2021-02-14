edc48e5f4b753d648743e36786f21fb6f33613bf
DennsHartstock <dennis.hartstock@web.de>
2021-02-14 10:09:21 +0100

    Как известно, на планете Земля есть 6 материков. Создай для каждого материка собственный класс. В каждом классе должно быть поле, которое хранит площадь соответствующего материка. Площадь материка нужно указывать при его создании, и она должна быть неизменяемой. Ты можешь использовать любые числа для задания площади материков.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task10.task1017;
+
+public class Earth {
+    public static void main(String[] args) {
+        new SouthAmerica(17_819_000);
+        new NorthAmerica(24_256_000);
+        new Eurasia(9_938_000 + 44_579_000);
+        new Australia(7_687_000);
+        new Antarctica(13_209_000);
+        new Africa(30_065_000);
+    }
+}
