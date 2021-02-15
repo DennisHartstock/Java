963a0c773ed325206a11e5f2bd3bb863a5736100
DennsHartstock <dennis.hartstock@web.de>
2021-02-15 08:42:23 +0100

    Для отображения звезды можно использовать любой символ из стандарта Юникод. Его будем хранить в поле STAR_SIGN класса Star. Чтобы отображать звезду на игровом поле, создадим метод draw(Game). В нем будем вызывать метод setCellValueEx у объекта типа Game, который получаем в качестве параметра. В метод setCellValueEx передадим координаты звезды, цвет фона, символ звезды, цвет и размер этого символа. Обрати внимание, что у звезды тип координат x и y — double, поэтому при вызове метода setCellValueEx их нужно привести к типу int. Чтобы добавить звезды в игру, создадим для них список и метод, который будет заполнять этот список новыми звездами. А для отображения звезд на игровом поле, нужно вызвать метод отрисовки draw(Game) у каждой звезды после отрисовки игрового поля.

--- JavaRushTasks/8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,10 +1,15 @@
 package com.javarush.games.spaceinvaders;
 
 import com.javarush.engine.cell.*;
+import com.javarush.games.spaceinvaders.gameobjects.Star;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpaceInvadersGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private List<Star> stars;
 
     @Override
     public void initialize() {
@@ -13,6 +18,7 @@ public class SpaceInvadersGame extends Game {
     }
 
     private void createGame() {
+        createStars();
         drawScene();
     }
 
@@ -21,10 +27,23 @@ public class SpaceInvadersGame extends Game {
     }
 
     private void drawField() {
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                setCellValueEx(x, y, Color.RED, "");
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                setCellValueEx(x, y, Color.BLACK, "");
             }
+        }
+
+        for (Star star : stars) {
+            star.draw(this);
+        }
+    }
+
+    private void createStars() {
+        stars = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            int x = getRandomNumber(WIDTH);
+            int y = getRandomNumber(HEIGHT);
+            stars.add(new Star(x, y));
         }
     }
 }
