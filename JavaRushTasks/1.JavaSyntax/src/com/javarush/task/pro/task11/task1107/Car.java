5c3652496d549eb84c76829f0271b0ff92e07b80
DennsHartstock <dennis.hartstock@web.de>
2021-02-16 09:13:16 +0100

    Двигатель — сложный механизм, для описания которого недостаточно обычной переменной. В классе Car создай внутренний класс (inner class) Engine. В классе Engine создай boolean поле isRunning, которое принимает значение true, если двигатель запущен, и false в противном случае. Также в этот класс добавь методы start и stop, которые будут запускать/останавливать двигатель (устанавливать соответствующее значение полю isRunning). Чтобы использовать двигатель, создай поле engine типа Engine в классе Car.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task11.task1107;
+
+
+public class Car {
+    private Engine engine;
+
+    class Engine {
+        private boolean isRunning;
+
+        public void start() {
+            isRunning = true;
+            System.out.println("Двигатель запущен");
+        }
+
+        public void stop() {
+            isRunning = false;
+            System.out.println("Двигатель остановлен");
+        }
+    }
+}
