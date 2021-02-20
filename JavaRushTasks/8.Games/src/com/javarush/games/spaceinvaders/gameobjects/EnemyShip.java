d2f72fe380e746895ff7063607af1f0b1a04b73b
DennsHartstock <dennis.hartstock@web.de>
2021-02-20 09:43:10 +0100

    В игре будет все двигаться, а значит, нам нужно определять направление движения объектов. Хранить направления удобно в enum'е. Движение вражеского корабля опишем в методе move. В зависимости от направления и скорости, он меняет соответствующую координату. У движения вниз скорость постоянная — 2. Поскольку вражеский флот движется от одного края игрового поля к другому, нам нужно контролировать достижение флотом боковых границ.
    Требования:
    1. В отдельном файле должен существовать публичный enum Direction со значениями: RIGHT, LEFT, UP, DOWN.
    2. В классе EnemyShip должен существовать публичный метод void move(Direction direction, double speed).
    3. В методе move(Direction direction, double speed) необходимо увеличить значение поля x на speed, если direction равно Direction.RIGHT.
    4. В методе move(Direction direction, double speed) необходимо уменьшить значение поля x на speed, если direction равно Direction.LEFT.
    5. В методе move(Direction direction, double speed) необходимо увеличить значение поля y на 2, если direction равно Direction.DOWN.
    6. В классе EnemyFleet должен существовать приватный метод double getLeftBorder().
    7. Метод getLeftBorder() должен возвращать минимальную координату x среди всех вражеских кораблей из списка ships.
    8. В классе EnemyFleet должен существовать приватный метод double getRightBorder().
    9. Метод getRightBorder() должен возвращать максимальное из значений (x + width) среди всех вражеских кораблей из списка ships.

--- JavaRushTasks/8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyShip.java
+++ JavaRushTasks/8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyShip.java
@@ -1,10 +1,19 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.games.spaceinvaders.Direction;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
 public class EnemyShip extends Ship {
     public EnemyShip(double x, double y) {
         super(x, y);
         setStaticView(ShapeMatrix.ENEMY);
+    }
+
+    public void move(Direction direction, double speed) {
+        switch (direction) {
+            case RIGHT -> x = x + speed;
+            case LEFT -> x = x - speed;
+            case DOWN -> y += 2;
+        }
     }
 }
